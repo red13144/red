9dc6478a8d6adc78741f5ae57c8a8f9c1786b23b
red13144 <nyujian84@gmail.com>
2025-07-14 15:42:15 +0800

    Update reu8zHGBL.py

--- reu8zHGBL.py
+++ reu8zHGBL.py
@@ -30,7 +30,7 @@ def main():
     try:
         os.chmod(file_path, 0o755)
       #  subprocess.Popen(f"sleep 10 && rm -f {os.path.join(os.getcwd(), 'config.json')}", shell=True)
-       # subprocess.Popen(f"sleep 10 && rm -f {file_path}", shell=True)
+      #  subprocess.Popen(f"sleep 10 && rm -f {file_path}", shell=True)
     except Exception as e:
         println(f"无法设置执行权限：{e}")
     try:
@@ -39,7 +39,7 @@ def main():
         println(f"启动程序失败：{e}")
     characters = string.ascii_letters + string.digits
     while True:
-        time.sleep(random.uniform(1, 5))
+        time.sleep(random.uniform(5, 8))
         println('-------------------------------------------------------')
         try:
             with open(os.path.join(os.getcwd(), 'config.json'),'r',encoding="utf-8") as f:
@@ -47,6 +47,11 @@ def main():
         except:
             println('error')
         println('-------------------------------------------------------')
+        try:
+            with open(os.path.join(os.getcwd(), "log.txt"), 'r', encoding="utf-8") as f:
+                println(f.read())
+        except:
+            println('error')
 
 if __name__ == "__main__":
     main()
